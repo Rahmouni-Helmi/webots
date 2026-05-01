ff2626c8badfd1f05b6d8e6eed8d66f535a3755a
hel310 <rahmounihilmi325@gmail.com>
2026-05-01 12:45:51 +0100

    v1

---
+++ controllers/belt_controller/belt_controller.py
@@ -0,0 +1,19 @@
+from controller import Robot
+
+# Init robot
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+
+# Get motor
+motor = robot.getDevice("belt motor")
+
+# Set velocity mode
+motor.setPosition(float('inf'))
+
+# Set speed
+speed = 0.2
+motor.setVelocity(speed)
+
+# Main loop
+while robot.step(timestep) != -1:
+    pass
